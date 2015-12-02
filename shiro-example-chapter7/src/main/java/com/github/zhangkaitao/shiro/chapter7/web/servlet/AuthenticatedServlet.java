75ed7f426104111966cc891d20bb3713357b4b70
dimhat <dimhat@qq.com>
2015-12-02 18:05:52 +0800

    chapter23

--- shiro-example-chapter7/src/main/java/com/github/zhangkaitao/shiro/chapter7/web/servlet/AuthenticatedServlet.java
+++ shiro-example-chapter7/src/main/java/com/github/zhangkaitao/shiro/chapter7/web/servlet/AuthenticatedServlet.java
@@ -1,14 +1,15 @@
 package com.github.zhangkaitao.shiro.chapter7.web.servlet;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 
 /**
  * <p>User: Zhang Kaitao
@@ -18,15 +19,15 @@ import java.io.IOException;
 @WebServlet(name = "authenticatedServlet", urlPatterns = "/authenticated")
 public class AuthenticatedServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Subject subject = SecurityUtils.getSubject();
-        if(subject.isAuthenticated()) {
-            req.getRequestDispatcher("/WEB-INF/jsp/authenticated.jsp").forward(req, resp);
-        } else {
-            req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req, resp);
-        }
-    }
-
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Subject subject = SecurityUtils.getSubject();
+		if (subject.isAuthenticated()) {
+			req.setAttribute("subject", subject);
+			req.getRequestDispatcher("/WEB-INF/jsp/authenticated.jsp").forward(req, resp);
+		} else {
+			req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req, resp);
+		}
+	}
 
 }
--- shiro-example-chapter7/src/main/java/com/github/zhangkaitao/shiro/chapter7/web/servlet/RoleServlet.java
+++ shiro-example-chapter7/src/main/java/com/github/zhangkaitao/shiro/chapter7/web/servlet/RoleServlet.java
@@ -1,14 +1,15 @@
 package com.github.zhangkaitao.shiro.chapter7.web.servlet;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 
 /**
  * <p>User: Zhang Kaitao
@@ -18,10 +19,10 @@ import java.io.IOException;
 @WebServlet(name = "roleServlet", urlPatterns = "/role")
 public class RoleServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Subject subject = SecurityUtils.getSubject();
-        subject.checkRole("admin");
-        req.getRequestDispatcher("/WEB-INF/jsp/hasRole.jsp").forward(req, resp);
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Subject subject = SecurityUtils.getSubject();
+		//subject.checkRole("admin");
+		req.getRequestDispatcher("/WEB-INF/jsp/hasRole.jsp").forward(req, resp);
+	}
 }
